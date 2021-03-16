c5cefead00f41182287f264afd5c4d889ef9518e
Jordi Ros <jordirm2000@gmail.com>
2021-03-16 12:46:40 +0100

    Language UI and id

---
+++ js/dictionary.js
@@ -0,0 +1,328 @@
+var dictionary = {
+    //nav
+    'nav-lang' : {
+        'ES' : 'es',
+        'VAL' : 'val',
+        'EN' : 'en'
+    },
+    'nav-car' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'nav-team' : {
+        'ES' : 'Nosotros',
+        'VAL' : 'Nosaltres',
+        'EN' : 'About Us'
+    },
+    'nav-sponsors' : {
+        'ES' : 'Patrocinadores',
+        'VAL' : 'Patrocinadors',
+        'EN' : 'Sponsors'
+    },
+    'nav-images' : {
+        'ES' : 'Imágenes',
+        'VAL' : 'Imatges',
+        'EN' : 'Images'
+    },
+    'contact-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'contact-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'contact-button' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'portfolio-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'portfolio-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+
+    //INDEX
+    'masthead-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'masthead-button' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    //Objectives
+    'objective1-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'objetive1-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'objective2-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'objetive2-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'objective3-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'objetive3-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    //La otra cara del proyecto
+    'objetive3-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'objetive3-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    //La otra cara del proyecto
+    'green-project-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'green-project-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+
+    //CAR
+    'adapted-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'adapted-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'doing-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'doing-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'Power' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'engines-number' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'traction' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'revolutions' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'capacity' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'weight' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'engine-torque' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'acceleration' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'autonomy' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'velocity' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'co2' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    //Sections-car
+    'engine-section-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'battery-section-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'design-section-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'marketing-section-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'engine-section-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'battery-section-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'design-section-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'marketing-section-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+
+    //IMAGES
+    'images-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+
+    //SPONSORS
+    'sponsors-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+
+    //TEAM
+    'team-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'coordinadores-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'cordinadores-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'cordinadores-engine-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'cordinadores-battery-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'cordinadores-design-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'cordinadores-marketing-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'engine-team-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'engine-team-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'battery-team-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'battery-team-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'design-team-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'design-team-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'marketing-team-title' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+    'marketing-team-text' : {
+        'ES' : 'Coche',
+        'VAL' : 'Cotxe',
+        'EN' : 'Car'
+    },
+
+}
