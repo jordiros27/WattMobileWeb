0ed4050a7d997985beca7289389f720aa2d20532
Jordi Ros <jordirm2000@gmail.com>
2021-03-20 20:25:44 +0100

    Dictionary

--- js/dictionary.js
+++ js/dictionary.js
@@ -2,7 +2,7 @@ var dictionary = {
     //nav
     'nav-lang' : {
         'ES' : 'ES',
-        'VAL' : 'VAL',
+        'VAL' : 'VLC',
         'EN' : 'EN'
     },
     'nav-car' : {
@@ -26,83 +26,89 @@ var dictionary = {
         'EN' : 'Images'
     },
     'contact-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Contacto',
+        'VAL' : 'Contacte',
+        'EN' : 'Contact'
     },
     'contact-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Quiere contactar con nosotros? No lo dude!',
+        'VAL' : 'Vol contactar amb nosaltres? No ho dubte!',
+        'EN' : 'Do you want to contact us? Do not hesitate!'
     },
     'contact-button' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Contacté!',
+        'VAL' : 'Contacte!',
+        'EN' : 'Contact!'
     },
     'portfolio-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Portfolio',
+        'VAL' : 'Portfoli',
+        'EN' : 'Portfolio'
     },
     'portfolio-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Descargue el Portfolio',
+        'VAL' : 'Descarrega el Portfoli',
+        'EN' : 'Download the Portfolio'
     },
 
     //INDEX
     'masthead-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : '100% eléctrico para competición!',
+        'VAL' : '100% elèctric per a competició!',
+        'EN' : '100% electric for competition!'
     },
     'masthead-button' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Conoce el coche',
+        'VAL' : 'Coneix el cotxe',
+        'EN' : 'Know the car'
     },
     //Objectives
     'objective1-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : '1r Objetivo',
+        'VAL' : '1r Objectiu',
+        'EN' : '1st Objective'
     },
     'objective1-text' : {
-        'ES' : 'Coche',
+        'ES' : 'Nuestro principal objetivo es llevar el coche en el que estamos \
+        trabajando a la próxima edición de subidas de montaña. Para ello estamos electrificando un ford Ka del 2000. \
+        La competición a su máximo nivel requiere de una autonomía de 10 minutos y una potencia superior \
+        a los 100 km de autonomía que permita completar la prueba.',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'Our main objective is to take the car we are working on to the next edition \
+        of mountain climbs. To do this, we are electrifying a Ford Ka from the 2000s. \
+        The competition, at its highest level, requires a range of 10 minutes of autonomy \
+        and a power greater than 100 km of autonomy to complete the test.'
     },
     'objective2-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : '2o Objetivo',
+        'VAL' : '2n Objectiu',
+        'EN' : '2nd Objective'
     },
     'objective2-text' : {
-        'ES' : 'Coche',
+        'ES' : 'Otro de los objetivos que caracterizan al proyecto Wattmobile UPV \
+        es dar una segunda vida a los coches de combustión. De este modo los vehículos podrían ser reutilizados \
+        del mismo modo que se remodelan elementos como los muebles “vintage”.',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'Another objective that characterizes the Wattmobile UPV project is to give \
+        combustion cars a second life. In this way, vehicles could be reused in the same way \
+        that elements such as "vintage" furniture are remodeled.'
     },
     'objective3-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : '3r Objetivo',
+        'VAL' : '3r Objectiu',
+        'EN' : '3rd Objective'
     },
     'objective3-text' : {
-        'ES' : 'Coche',
+        'ES' : 'En el automovilismo español todavía no hay una competición de vehículos eléctricos de carreras. \
+        Es por ello que en Wattmobile UPV hemos querido emprender introduciendo en el deporte del motor \
+        la movilidad eléctrica. <b>De esta forma crear la primera competición con vehículos de competición 100% eléctricos. </b>\
+        Y así ser unos de los referentes a nivel europeo e internacional.',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
-    },
-    //La otra cara del proyecto
-    'objetive3-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
-    },
-    'objetive3-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'In Spanish motorsports there is still no competition for electric racing \
+        vehicles. That is why at WattmobileUPV we wanted to start by introducing electric \
+        mobility in motor sport. <b>In this way, we want to create the first competition with \
+        100% electric competition vehicles</b>, and thus be one of the benchmarks at an European \
+        and international level.'
     },
     //La otra cara del proyecto
     'green-project-title' : {
@@ -138,196 +144,228 @@ var dictionary = {
         'EN' : 'Car'
     },
     'power' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Potencia',
+        'VAL' : 'Potència',
+        'EN' : 'Power'
     },
     'engines-number' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Número de motores',
+        'VAL' : 'Nombre de motors',
+        'EN' : 'Engine number'
     },
     'traction' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Tracción',
+        'VAL' : 'Tracció',
+        'EN' : 'Traction'
     },
     'traction-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Delantera',
+        'VAL' : 'Davantera',
+        'EN' : 'Lead'
     },
     'revolutions' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Revoluciones',
+        'VAL' : 'Revolucions',
+        'EN' : 'Revolutions'
     },
     'capacity' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Capacidad',
+        'VAL' : 'Capacitat',
+        'EN' : 'Capacity'
     },
     'weight' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Peso',
+        'VAL' : 'Pes',
+        'EN' : 'Weight'
     },
     'engine-torque' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Par motor',
+        'VAL' : 'Parell motor',
+        'EN' : 'Engine torque'
     },
     'acceleration' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Aceleración',
+        'VAL' : 'Aceleració',
+        'EN' : 'Acceleration'
     },
     'autonomy' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Autonomía',
+        'VAL' : 'Autonomia',
+        'EN' : 'Autonomy'
     },
     'velocity' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Velocidad',
+        'VAL' : 'Velocitat',
+        'EN' : 'Velocity'
     },
     'co2' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'co2',
+        'VAL' : 'co2',
+        'EN' : 'co2'
     },
     //Sections-car
     'engine-section-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Motor',
+        'VAL' : 'Motor',
+        'EN' : 'Engine'
     },
     'battery-section-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Batería',
+        'VAL' : 'Bateria',
+        'EN' : 'Battery'
     },
     'design-section-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Diseño',
+        'VAL' : 'Disseny',
+        'EN' : 'Design'
     },
     'marketing-section-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Marketing',
+        'VAL' : 'Marketing',
+        'EN' : 'Marketing'
     },
     'engine-section-text' : {
-        'ES' : 'Coche',
+        'ES' : 'Para la unidad de potencia hemos empleado un motor de flujo axial. Gracias \
+        a UNITEK Industrie Elektronik hemos podido controlar el motor dándonos unos magníficos \
+        150CV. En el grupo de motor se trabaja en aspectos dinámicos del coche y estructurales. \
+        Tales como el diseño del bloque motor y su integración en el vehículo. En este departamento \
+        se aplican conocimientos de electrónica, eléctrica, mecánica, informática, etc.',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'For the power unit we have used an axial flow motor. Thanks to UNITEK \
+        Industrie Elektronik we have been able to control the engine, giving us a magnificent \
+        150CV. The engine group works on dynamic and structural aspects of the car, such as the \
+        design of the engine block and its integration into the vehicle. In this department, \
+        knowledge of electronics, electrical, mechanics, computing, etc., is applied.'
     },
     'battery-section-text' : {
-        'ES' : 'Coche',
+        'ES' : 'La batería es una de las piezas clave del proyecto, con ella podemos alimentar a \
+        todos los dispositivos del vehículo. Hemos diseñado un sistema de 12 baterías de 48v con \
+        los cuales obtenemos unos 670V en su estado de máxima carga y proporcionando una potencia de 150KW.\
+        <br><br>Tanto la búsqueda de material como la creación de baterías para vehículos tienen lugar en la UPV. \
+        En WattmobileUPV hemos adquirido un conocimiento sobre la tecnología de baterías que nos permite \
+        fabricar nuestros propios modelos. En este departamento se aplican conocimientos de electrónica, \
+        eléctrica, mecánica, informática y diseño entre otras disciplinas.',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'The battery is one of the key pieces of the project, with it we can power \
+        all the devices of the vehicle. We have designed a system of 12 48V batteries with \
+        which we obtain about 670V in its state of maximum charge and providing a power of 150KW.\
+        <br><br>Both the search for material and the creation of batteries for vehicles take place at \
+        the UPV. At WattmobileUPV we have acquired a knowledge of battery technology that allows us \
+        to manufacture our own models. In this department, knowledge of electronics, electrical, \
+        mechanics, computing and design, among other disciplines, is applied.'
     },
     'design-section-text' : {
-        'ES' : 'Coche',
+        'ES' : 'En este equipo se trabaja la imagen del proyecto siendo así la mirada que construye \
+        hacía nuestro público.\
+        <br><br>Las principales funciones son el diseño de la página web, de las redes sociales, el exterior \
+        del coche y el centro de información dentro del coche. Como se puede ver, este grupo se enfoca \
+        en la parte más creativa y espontánea del proyecto',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'In this team the image of the project is worked on, thus being the look it builds \
+        towards our public.\
+        <br><br>The main functions are the design of the website, social networks, the exterior of the car \
+        and the information center inside the car. As it can be seen, this group focuses on the most\
+         creative and spontaneous part of the project.'
     },
     'marketing-section-text' : {
-        'ES' : 'Coche',
+        'ES' : 'El grupo de marketing se encarga de toda la gestión del proyecto. Se tratan asuntos \
+        económicos, sociales y organizativos. El principal objetivo de esta sección es el funcionamiento \
+        adecuado de las distintas partes que componen WattmobileUPV.',
         'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'EN' : 'The marketing group is in charge of all project management. Economic, social and \
+        organizational issues are discussed. The main objective of this section is the proper functioning \
+        of the different parts that make up WattmobileUPV.'
     },
 
     //IMAGES
     'images-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Imágenes',
+        'VAL' : 'Imatges',
+        'EN' : 'Images'
     },
 
     //SPONSORS
     'sponsors-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Patrocinadores',
+        'VAL' : 'Patrocinadors',
+        'EN' : 'Sponsors'
     },
 
     //TEAM
     'team-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Equipo',
+        'VAL' : 'Equip',
+        'EN' : 'Team'
     },
     'coordinadores-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Coordinadores',
+        'VAL' : 'Coordiandors',
+        'EN' : 'Coordinators'
     },
     'coordinadores-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Coordinador del proyecto',
+        'VAL' : 'Coordinador del projecte',
+        'EN' : 'Project coordinator'
     },
     'coordinadores-engine-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Coordinador de Motor',
+        'VAL' : 'Coordinador de Motor',
+        'EN' : 'Engine coordinator'
     },
     'coordinadores-battery-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Coordinador de Batería',
+        'VAL' : 'Coordinador de Bateria',
+        'EN' : 'Battery coordinator'
     },
     'coordinadores-design-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Coordinador de Diseño',
+        'VAL' : 'Coordinador de Disseny',
+        'EN' : 'Design coordinator'
     },
     'coordinadores-marketing-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Coordinador de Marketing',
+        'VAL' : 'Coordinador de Marketing',
+        'EN' : 'Marketing coordiantor'
     },
     'engine-team-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Motor',
+        'VAL' : 'Motor',
+        'EN' : 'Engine'
     },
     'engine-team-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Miembro de Motor',
+        'VAL' : 'Membre de Motor',
+        'EN' : 'Engine member'
     },
     'battery-team-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Batería',
+        'VAL' : 'Bateria',
+        'EN' : 'Battery'
     },
     'battery-team-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Miembro de Batería',
+        'VAL' : 'Membre de Bateria',
+        'EN' : 'Battery member'
     },
     'design-team-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Diseño',
+        'VAL' : 'Disseny',
+        'EN' : 'Design'
     },
     'design-team-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Miembro de Diseño',
+        'VAL' : 'Membre de Disseny',
+        'EN' : 'Design member'
     },
     'marketing-team-title' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Marketing',
+        'VAL' : 'Marketing',
+        'EN' : 'Marketing'
     },
     'marketing-team-text' : {
-        'ES' : 'Coche',
-        'VAL' : 'Cotxe',
-        'EN' : 'Car'
+        'ES' : 'Miembro de Marketing',
+        'VAL' : 'Membre de Marketing',
+        'EN' : 'Marketing member'
     },
 
 }
